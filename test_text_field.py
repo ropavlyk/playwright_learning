d5f65a10c43bc80da666628b8f7aafa8cc041a26
Roman Pavlyk <rpavl@Romans-MacBook-Air.local>
2026-05-15 15:08:54 -0600

    added text field tests

---
+++ test_text_field.py
@@ -0,0 +1,26 @@
+from playwright.sync_api import sync_playwright, expect
+
+def test_text_field(): 
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+    
+        page.goto("https://demoqa.com/text-box")
+
+        page.locator("#userName").fill("Roman Roman") 
+        page.locator("#userEmail").fill("roman@example.com")
+        page.locator("#currentAddress").fill("Vancouver")
+        page.locator("#permanentAddress").fill("Calgary")
+        
+
+        page.get_by_role("button", name="Submit").click() 
+        #page.locator("#submit").click()
+
+        expect(page.get_by_text("Name:Roman Roman")).to_be_visible()
+        expect(page.get_by_text("Email:roman@example.com")).to_be_visible()
+        expect(page.get_by_text("Current Address :Vancouver")).to_be_visible()
+        expect(page.get_by_text("Permananet Address :Calgary")).to_be_visible()
+        
+        browser.close()
+
+        #run pytest in terminal: pytest Learning/test_text_field.py -v
