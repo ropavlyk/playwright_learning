58f18f6fa13831cddafc76b39ae9aa5b56d4e242
Roman Pavlyk <ropavlyk@gmail.com>
2026-05-15 17:39:26 -0600

    added radio button tests

---
+++ test_radio_button.py
@@ -0,0 +1,42 @@
+from playwright.sync_api import sync_playwright, expect
+
+def test_yes_radio_button(): 
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+    
+        page.goto("https://demoqa.com/radio-button")
+
+        page.locator("#yesRadio").click()
+        expect(page.get_by_text("You have selected Yes")).to_be_visible()
+        
+        browser.close()
+
+
+
+def test_impressive_radio_button():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+    
+        page.goto("https://demoqa.com/radio-button")
+
+        #page.locator("#impressiveRadio").click()
+        page.get_by_role("radio", name="Impressive").click()
+        expect(page.get_by_text("You have selected Impressive")).to_be_visible()
+
+        browser.close()
+
+
+
+def test_disabled_radio_button():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+    
+        page.goto("https://demoqa.com/radio-button")
+        expect(page.locator("#noRadio")).to_be_disabled()
+
+        browser.close()
+
+        #run in terminal: pytest Learning/test_radio_button.py -v
