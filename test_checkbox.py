b5ca00a0db493d72e83657d7c8668de508042e28
Roman Pavlyk <ropavlyk@gmail.com>
2026-05-15 17:31:35 -0600

    added checkbox tests

---
+++ test_checkbox.py
@@ -0,0 +1,68 @@
+from playwright.sync_api import sync_playwright, expect
+
+def test_checkbox_checks():
+    with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+        
+            page.goto("https://demoqa.com/checkbox")
+            page.get_by_role("checkbox", name="Select Home").check()
+            #page.get_by_label("Home").click()
+
+            expect(page.get_by_text("You have selected :home")).to_be_visible()
+
+            browser.close()
+
+
+def test_checkbox_unchecks():
+    with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+        
+            page.goto("https://demoqa.com/checkbox")
+            page.get_by_role("checkbox", name="Select Home").click()
+            expect(page.get_by_text("You have selected :home")).to_be_visible()
+            page.get_by_role("checkbox", name="Select Home").click()
+            expect(page.get_by_text("You have selected :home")).not_to_be_visible()
+
+            browser.close()
+
+
+def test_tree_expands():
+    with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+        
+            page.goto("https://demoqa.com/checkbox")
+            page.locator(".rc-tree-switcher").click() 
+            expect(page.get_by_role("checkbox", name="Select Desktop")).to_be_visible()
+
+            browser.close()
+
+
+def test_tree_collapses():
+    with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+        
+            page.goto("https://demoqa.com/checkbox")
+            page.locator(".rc-tree-switcher").click()
+            expect(page.get_by_role("checkbox", name="Select Desktop")).to_be_visible()
+            page.locator(".rc-tree-switcher_open").click()
+            expect(page.get_by_role("checkbox", name="Select Desktop")).not_to_be_visible()
+
+            browser.close()
+
+
+def test_sub_checkbox_checks():
+    with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+
+            page.goto("https://demoqa.com/checkbox")
+            page.locator(".rc-tree-switcher").click() 
+            expect(page.get_by_role("checkbox", name="Select Desktop")).to_be_visible()
+            page.get_by_role("checkbox", name="Select Desktop").click()
+            expect(page.get_by_text("You have selected :desktop")).to_be_visible()
+
+            browser.close()
