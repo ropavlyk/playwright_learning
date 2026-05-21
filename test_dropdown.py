9d69b4bf1e0890af63ff343b8b960fcf0e4eda8c
Roman Pavlyk <ropavlyk@gmail.com>
2026-05-20 20:19:40 -0600

    added dropdown tests

---
+++ test_dropdown.py
@@ -0,0 +1,47 @@
+from playwright.sync_api import sync_playwright, expect
+
+def test_standard_dropdown():
+    with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+        
+            page.goto("https://demoqa.com/select-menu")
+
+            page.locator("#oldSelectMenu").select_option("Black")
+
+            expect(page.locator("#oldSelectMenu")).to_have_value("5")
+
+            browser.close()
+
+
+def test_custom_dropdown():
+    with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+        
+            page.goto("https://demoqa.com/select-menu")
+
+            page.locator("#selectOne").click()
+            page.locator("#react-select-3-option-0-1").click()
+            expect(page.locator("#selectOne")).to_contain_text("Mr.")
+
+            #page.wait_for_timeout(2000) 
+
+            browser.close()
+
+
+def test_custom_multiselect_dropdown():
+    with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+        
+            page.goto("https://demoqa.com/select-menu")
+
+            page.locator("#react-select-4-input").click()
+            page.locator("#react-select-4-option-2").click()
+            page.locator("#react-select-4-option-3").click()
+
+            expect(page.locator(".css-9jq23d", has_text="Black")).to_be_visible()
+            expect(page.locator(".css-9jq23d", has_text="Red")).to_be_visible()
+
+            browser.close()
